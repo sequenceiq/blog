f234c1dffcbae076ed414b1ff50a9e6f4273c057
ec2-machine <lalyos@yahoo.com>
2013-11-22 17:19:05 +0000

    site config changed

--- create_post.sh
+++ create_post.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
 export PATH="/usr/local/rbenv/shims:$HOME/.rbenv/bin:$PATH"
 
-TITLE="$*"
 
+cd /home/ubuntu/seq-blog
+TITLE="$*"
 rake new_post["${TITLE}"]
+
+git add source
+git commit -m "[script] blog created: $TITLE"
+git push hub master
