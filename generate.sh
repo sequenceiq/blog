8cb70308e8a0742a8c7ca4df26578fa567e75fc9
ec2-machine <lalyos@yahoo.com>
2013-11-22 17:48:47 +0000

    generate script fixed

--- generate.sh
+++ generate.sh
@@ -4,7 +4,7 @@ export PATH="/usr/local/rbenv/shims:$HOME/.rbenv/bin:$PATH"
 cd /home/ubuntu/seq-blog
 
 git stash
-git pull --rebase
+git pull master --rebase
 rake generate
 rake deploy
 git stash pop
